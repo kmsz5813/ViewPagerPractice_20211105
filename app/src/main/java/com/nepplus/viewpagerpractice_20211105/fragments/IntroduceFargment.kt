486e22d713e8a888726e0f6a792ebc626155eb7d
김민성 <kmsz5813@naver.com>
2021-11-05 12:44:23 +0900

    이름 / 소개 프래그먼트 추가  - 복사 / 붙여넣기를 활용

---
+++ app/src/main/java/com/nepplus/viewpagerpractice_20211105/fragments/IntroduceFargment.kt
@@ -0,0 +1,25 @@
+package com.nepplus.viewpagerpractice_20211105.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nepplus.viewpagerpractice_20211105.R
+
+class IntroduceFargment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_introduce, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
+}
